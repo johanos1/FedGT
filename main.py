bac2c96dea8062f8880234df3b95c1a1787994b0
johanos1 <oshtman@gmail.com>
2022-10-26 00:05:47 +0200

    Updated to fedML flow

--- main.py
+++ main.py
@@ -1,112 +1,167 @@
 import torch
-import torchvision  # type: ignore
-import torch.nn as tn
-from torchvision.transforms import ToTensor  # type: ignore
-from torch.utils.data import random_split, DataLoader
-import torch.nn.functional as F
-import copy
-from loguru import logger
-from ctypes import CDLL
+import numpy as np
+import random
 
-input_size = 28*28  # Size of image
-num_classes = 10  # the image number are in range 0-10
-num_epochs = 1
-batch_size = 100  # sample size consider before updating the model’s weights
-learning_rate = 0.001  # step size to update parameter
+import data_preprocessing.data_loader as dl
+import argparse
+from models.resnet import resnet56, resnet18
+from models.multinomial_log_reg import multi_nomial_regression
+from torch.multiprocessing import set_start_method, Queue
+import logging
+import os
+from collections import defaultdict
+import time
 
+# methods
+import methods.fedavg as fedavg
+#https://stackoverflow.com/questions/6974695/python-process-pool-non-daemonic#:~:text=As%20of%20Python,answer%20by%20jfs
+from concurrent.futures import ProcessPoolExecutor as Pool
 
-class MultinomialLogisticRegression(tn.Module):
+# Helper Functions
+def init_process(q, Client):
+    set_random_seed()
+    global client
+    ci = q.get()
+    client = Client(ci[0], ci[1])
 
-    def __init__(self, input_size, num_classes):
-        super().__init__()
-        self.linear = tn.Linear(input_size, num_classes, bias=False)
+def run_clients(received_info):
+    try:
+        return client.run(received_info)
+    except KeyboardInterrupt:
+        logging.info('exiting')
+        return None
 
-    def forward(self, feature):
-        feature = feature.reshape(-1, 784)  # flatten because tensor is 1x28x28
-        output = self.linear(feature)
-        
-        return torch.softmax(output)
-
-    def get_gradients(self):
-        gradient_list = []
-        for item in self.parameters():
-            gradient_list.append(copy.deepcopy(item.grad))
-
-    def training_step(self, batch):
-        images, labels = batch
-        out = self(images)                  # Generate predictions
-        loss = F.cross_entropy(out, labels)  # Calculate loss. Note: cross entropy first computes a softmax and then the crossentropy
-        return loss
-
-    def validation_step(self, batch):
-        images, labels = batch
-        out = self(images)                    # Generate predictions
-        loss = F.cross_entropy(out, labels)   # Calculate loss
-        acc = accuracy(out, labels)           # Calculate accuracy
-        return {'loss': loss.detach(), 'acc': acc.detach()}
-
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x['loss'] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
-        batch_accs = [x['acc'] for x in outputs]
-        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
-        return {'loss': epoch_loss.item(), 'acc': epoch_acc.item()}
-
-    def epoch_end(self, epoch, result):
-        logger.info("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['loss'], result['acc']))
+def allocate_clients_to_threads(args):
+    mapping_dict = defaultdict(list)
+    for round in range(args.comm_round):
+        if args.client_sample<1.0:
+            num_clients = int(args.client_number*args.client_sample)
+            client_list = random.sample(range(args.client_number), num_clients)
+        else:
+            num_clients = args.client_number
+            client_list = list(range(num_clients))
+        if num_clients % args.thread_number==0 and num_clients>0:
+            clients_per_thread = int(num_clients/args.thread_number)
+            for c, t in enumerate(range(0, num_clients, clients_per_thread)):
+                idxs = [client_list[x] for x in range(t, t+clients_per_thread)]
+                mapping_dict[c].append(idxs)
+        else:
+            raise ValueError("Sampled client number not divisible by number of threads")
+    return mapping_dict
 
 
-def accuracy(outputs, labels):
-    _, preds = torch.max(outputs, dim=1)
-    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+def add_args(parser):
+    # Training settings
+    parser.add_argument('--method', type=str, default='fedavg', metavar='N',
+                        help='Options are: fedavg, fedprox, moon, mixup, stochdepth, gradaug, fedalign')
+
+    parser.add_argument('--data_dir', type=str, default='data/cifar10',
+                        help='data directory: data/cifar100, data/cifar10, or another dataset')
+
+    parser.add_argument('--partition_method', type=str, default='homo', metavar='N',
+                        help='how to partition the dataset on local clients')
+
+    parser.add_argument('--partition_alpha', type=float, default=0.5, metavar='PA',
+                        help='alpha value for Dirichlet distribution partitioning of data(default: 0.5)')
+
+    parser.add_argument('--client_number', type=int, default=32, metavar='NN',
+                        help='number of clients in the FL system')
+
+    parser.add_argument('--batch_size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    
+    parser.add_argument('--wd', help='weight decay parameter;', type=float, default=0.0001)
+
+    parser.add_argument('--epochs', type=int, default=1, metavar='EP',
+                        help='how many epochs will be trained locally per round')
+
+    parser.add_argument('--comm_round', type=int, default=25,
+                        help='how many rounds of communications are conducted')
+
+    parser.add_argument('--pretrained', action='store_true', default=False,
+                        help='test pretrained model')
+
+    parser.add_argument('--save_client', action='store_true', default=False,
+                        help='Save client checkpoints each round')
+
+    parser.add_argument('--thread_number', type=int, default=16, metavar='NN',
+                        help='number of parallel training threads')
+
+    parser.add_argument('--client_sample', type=float, default=1.0, metavar='MT',
+                        help='Fraction of clients to sample')
+
+    args = parser.parse_args()
+
+    return args
 
 
-def evaluate(model, val_loader):
-    outputs = [model.validation_step(batch) for batch in val_loader]
-    return model.validation_epoch_end(outputs)
-
-
-def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
-    optimizer = opt_func(model.parameters(), lr)
-    for epoch in range(epochs):
-        # Training Phase (go through all batches)
-        for batch in train_loader:
-            loss = model.training_step(batch)
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-        # Validation phase
-        result = evaluate(model, val_loader)
-        model.epoch_end(epoch, result)
-
-
-def main():
-    #  load the MNIST dataset and put images into tensors
-    dataset = torchvision.datasets.MNIST('data',
-                                         train=True,
-                                         transform=ToTensor())
-    train_ds, val_ds = random_split(dataset, [50000, 10000])
-    test_ds = torchvision.datasets.MNIST('data',
-                                         train=False,
-                                         transform=ToTensor())
-
-    #  make data into iterables
-    train_loader = DataLoader(train_ds, batch_size, shuffle=True)
-    val_loader = DataLoader(val_ds, batch_size)
-    test_loader = DataLoader(test_ds, batch_size)
-
-    #  Create model and attach loss function and optimizer
-    model = MultinomialLogisticRegression(input_size, num_classes)
-    fit(num_epochs, learning_rate, model, train_loader, val_loader)
-    test_result = evaluate(model, test_loader)
-    accuracy = test_result['acc']
-    logger.info(f'Test accuracy: {accuracy}')
-
-
+# Setup Functions
+def set_random_seed(seed=1):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    ## NOTE: If you want every run to be exactly the same each time
+    ##       uncomment the following lines
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
+    
 if __name__ == "__main__":
-    main()
+    try:
+        set_start_method('spawn')
+    except RuntimeError:
+        pass
+    set_random_seed()
+    # get arguments
+    parser = argparse.ArgumentParser()
+    args = add_args(parser)
+ 
+    # get data
+    train_data_num, test_data_num, train_data_global, test_data_global, data_local_num_dict, train_data_local_dict, test_data_local_dict,\
+         class_num = dl.load_partition_data(args.data_dir, args.partition_method, args.partition_alpha, args.client_number, args.batch_size)
 
-    #  Test calling c file
-    so_file = "./src/C_code/my_functions.so"
-    my_functions = CDLL(so_file)
-    print(my_functions.square(10))
+    mapping_dict = allocate_clients_to_threads(args)
+    #init method and model type
+    if args.method=='fedavg':
+        Server = fedavg.Server
+        Client = fedavg.Client
+        #Model = resnet56 if 'cifar' in args.data_dir else resnet18
+        Model = multi_nomial_regression
+        server_dict = {'train_data':train_data_global, 'test_data': test_data_global, 'model_type': Model, 'num_classes': class_num}
+        client_dict = [{'train_data':train_data_local_dict, 'test_data': test_data_local_dict,  
+                        'device': 'cuda:{}'.format(i % torch.cuda.device_count()) if torch.cuda.is_available() else "cpu",
+                        'client_map':mapping_dict[i], 'model_type': Model, 'num_classes': class_num} for i in range(args.thread_number)]
+    
+    #init nodes
+    client_info = Queue()
+    for i in range(args.thread_number):
+        client_info.put((client_dict[i], args))
+
+    # Start server and get initial outputs
+    #pool = cm.MyPool(args.thread_number, init_process, (client_info, Client))
+    
+    # init server
+    server_dict['save_path'] = '{}/logs/{}__{}_e{}_c{}'.format(os.getcwd(),
+        time.strftime("%Y%m%d_%H%M%S"), args.method, args.epochs, args.client_number)
+    if not os.path.exists(server_dict['save_path']):
+        os.makedirs(server_dict['save_path'])
+    server = Server(server_dict, args)
+    server_outputs = server.start()
+    
+    # each thread will create a client object containing the client information
+    with Pool(max_workers=args.thread_number, initializer = init_process, initargs = (client_info, Client)) as pool: 
+        for r in range(args.comm_round):
+            logging.info('************** Round: {} ***************'.format(r))
+            round_start = time.time()
+            client_outputs = pool.map(run_clients, server_outputs)
+                # test
+                # init_process(client_info, Client)
+                # client.run(server_outputs[0])
+            client_outputs = [c for sublist in client_outputs for c in sublist]
+            server_outputs = server.run(client_outputs)
+            round_end = time.time()
+            logging.info('Round {} Time: {}s'.format(r, round_end-round_start))
