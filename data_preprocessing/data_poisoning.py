e23492ad101e25a2f7d4ae971a52191f988c9037
johanos1 <oshtman@gmail.com>
2022-11-01 09:54:14 +0100

    Docstring for data poisoning

--- data_preprocessing/data_poisoning.py
+++ data_preprocessing/data_poisoning.py
@@ -5,6 +5,15 @@ from typing import List, Tuple
 
 
 def flip_label(label_flips: List[Tuple[int, int]], ds: data.Dataset) -> data.Dataset:
+    """flip labels in dataset
+
+    Args:
+        label_flips (List[Tuple[int, int]]): list of labels to be flipped and its new value
+        ds (data.Dataset): dataset to poison
+
+    Returns:
+        data.Dataset: posioned dataset
+    """
 
     target_array = ds.target.cpu().detach().numpy()
     for (old_label, new_label) in label_flips:
@@ -14,6 +23,14 @@ def flip_label(label_flips: List[Tuple[int, int]], ds: data.Dataset) -> data.Dat
 
 
 def random_labels(ds: data.Dataset) -> data.Dataset:
+    """randomly assign labels in dataset
+
+    Args:
+        ds (data.Dataset): dataset to poison
+
+    Returns:
+        data.Dataset: posioned dataset
+    """
     n_labels = ds.target.shape[0]
     num_classes = len(np.unique(ds.target))
     ds.target = torch.from_numpy(np.random.randint(num_classes, size=(n_labels,)))
