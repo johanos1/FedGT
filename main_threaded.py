43bcabdb5734e8b457edb634b3f7d11875fd96a3
johanos1 <oshtman@gmail.com>
2023-04-14 13:19:38 +0200

    Remove duplicate code.

--- main_threaded.py
+++ main_threaded.py
@@ -402,13 +402,9 @@ if __name__ == "__main__":
                                     elif ATTACK == 2:
                                         DEC = gt.perform_group_test(rec[:, src])
 
-                                    DEC = gt.perform_group_test(group_accuracies)
                                 MD = MD + np.sum(gt.DEC[defective == 1] == 0)
                                 FA = FA + np.sum(gt.DEC[defective == 0] == 1)
-                                if (
-                                    np.sum(DEC) == DEC.shape[1]
-                                ):  # , "All are classified as malicious"
-                                    all_class_malicious = True
+                                if np.sum(DEC) == DEC.shape[1]: all_class_malicious = True
                                     
                                 sim_result["group_acc"][thres_indx, monte_carlo_iterr, :] = group_accuracies
                                 sim_result["group_prec"][thres_indx, monte_carlo_iterr, :, :] = prec
@@ -428,36 +424,18 @@ if __name__ == "__main__":
                                     for client_idx in range(cfg.Sim.n_clients)
                                     if DEC[:, client_idx] == 0
                                 ]
-                            # -----------------------------------------
-                            #               Aggregation
-                            # -----------------------------------------
-                            # If all malicious, just use all
-                            if all_class_malicious == True:
-                                clients_to_aggregate = client_outputs
-                            else:
-                                clients_to_aggregate = [
-                                    client_outputs[client_idx]
-                                    for client_idx in range(cfg.Sim.n_clients)
-                                    if DEC[:, client_idx] == 0
-                                ]
-                        server_outputs, acc[0, r], cf_matrix = server.run(
-                            clients_to_aggregate
-                        )
+                        server_outputs, acc[0, r], cf_matrix = server.run(clients_to_aggregate)
 
-                        sim_result["cf_matrix"][
-                            thres_indx, monte_carlo_iterr, r, :, :
-                        ] = cf_matrix
+                        sim_result["cf_matrix"][thres_indx, monte_carlo_iterr, r, :, :] = cf_matrix
 
                         round_end = time.time()
                         logging.info(f"Threshold: {threshold_dec} --- MC-Iteration: {monte_carlo_iterr} --- Round {r} ---  Time: {round_end - round_start} --- Accuracy: {acc[0,r]}")
 
                     accuracy[thres_indx, monte_carlo_iterr, :] = acc
 
-            if n_malicious > 0:
+            if n_malicious > 0: 
                 P_MD[thres_indx] = MD / (n_malicious * cfg.Sim.total_MC_it)
-            P_FA[thres_indx] = FA / (
-                (cfg.Sim.n_clients - n_malicious) * cfg.Sim.total_MC_it
-            )
+            P_FA[thres_indx] = FA / ((cfg.Sim.n_clients - n_malicious) * cfg.Sim.total_MC_it)
 
             # make all nparrays JSON serializable
             checkpoint_dict = copy.deepcopy(sim_result)
