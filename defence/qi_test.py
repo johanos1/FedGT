d54ea98a8afe5c55be1f88b0295e309b399b5640
AMD\PIDZSO <balazs.pejo@gmail.com>
2024-10-18 14:52:30 +0200

    QI across fix

--- defence/qi_test.py
+++ defence/qi_test.py
@@ -12,12 +12,13 @@ class QI_Test():
         assert self.n_tests == self.groups4test.shape[0], "Wrong no of rows in H!"
         assert self.n_clients == self.groups4test.shape[1], "Wrong no of cols in H!"
 
-    def scoring(self, norm, scores, badgroup, goodgroup):
+    def scoring(self, norm, scores, badgroup, goodgroup=None):
         for u in range(self.n_clients):
             if self.groups4test[badgroup][u]:
                 scores[u] -= 1 / norm[u]
-            if self.groups4test[goodgroup][u]:
-                scores[u] += 1 / norm[u]
+            if goodgroup is not None:
+                if self.groups4test[goodgroup][u]:
+                    scores[u] += 1 / norm[u]
         return scores
 
     def perform_QI_test_inround(self, group_acc, r):
@@ -35,7 +36,9 @@ class QI_Test():
         normalize = np.sum(self.groups4test, 0)
         scores = np.zeros(self.n_clients)
         for j in range(group_imp.shape[1]):
-            for k in range(j+1, group_imp.shape[1]):
+            for k in range(group_imp.shape[1]):
                 if group_imp[r-1][j] < group_imp[r][k] - self.threshold:
                     self.scoring(normalize, scores, j, k)
+            if group_imp[r][j] < 0 - self.threshold:  # not checking the first test-round
+                self.scoring(normalize, scores, j)
         return scores
