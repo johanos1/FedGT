a2ef1ebd0b771e178e9a0ca87ed6f34a7b17bde5
AMD\PIDZSO <balazs.pejo@gmail.com>
2025-03-19 10:45:32 +0100

    debug

--- defence/qi_test.py
+++ defence/qi_test.py
@@ -16,37 +16,40 @@ class QI_Test():
         assert self.n_tests == self.groups4test_curr.shape[0], "Wrong no of rows in H!"
         assert self.n_clients == self.groups4test_curr.shape[1], "Wrong no of cols in H!"
 
-    def scoring(self, norm, scores, badgroup, goodgroup=None, past=None):
+    def scoring_across(self, norm, scores, badgroup, goodgroup=None):
+        for u in range(self.n_clients):
+            if self.groups4test_past[badgroup][u]:
+                scores[u] -= 1 / norm[u]
+            if goodgroup is not None and self.groups4test_curr[goodgroup][u]:
+                scores[u] += 1 / norm[u]
+        return scores
+
+    def scoring_inround(self, norm, scores, badgroup, goodgroup=None):
         for u in range(self.n_clients):
             if self.groups4test_curr[badgroup][u]:
                 scores[u] -= 1 / norm[u]
-            if goodgroup is not None:
-                if past is not None:
-                    if self.groups4test_past[goodgroup][u]:
-                        scores[u] += 1 / norm[u]
-                else:
-                    if self.groups4test_curr[goodgroup][u]:
-                        scores[u] += 1 / norm[u]
+            if goodgroup is not None and self.groups4test_curr[goodgroup][u]:
+                scores[u] += 1 / norm[u]
         return scores
 
-    def perform_QI_test_inround(self, group_acc, r):
+    def perform_QI_test_inround(self, group_imp, r):
         normalize = np.sum(self.groups4test_curr, 0)
         scores = np.zeros(self.n_clients)
-        for j in range(group_acc.shape[1]):
-            for k in range(j+1, group_acc.shape[1]):
-                if group_acc[r][j] < group_acc[r][k] - self.threshold:
-                    self.scoring(normalize, scores, j, k)
+        for j in range(group_imp.shape[1]):
+            if group_imp[r][j] < 0 - self.threshold:
+                self.scoring_inround(normalize, scores, j)
+            for k in range(group_imp.shape[1]):
+                if group_imp[r][j] < group_imp[r][k] - self.threshold:
+                    self.scoring_inround(normalize, scores, j, k)
         return scores
 
     def perform_QI_test_acrossround(self, group_imp, r):
         normalize = np.sum(self.groups4test_curr, 0) + np.sum(self.groups4test_past, 0)
         scores = np.zeros(self.n_clients)
         for j in range(group_imp.shape[1]):
+            if group_imp[r][j] < 0 - self.threshold:
+                self.scoring_across(normalize, scores, j)
             for k in range(group_imp.shape[1]):
-                print(group_imp[r-1][j], group_imp[r][k])
                 if group_imp[r-1][j] < group_imp[r][k] - self.threshold:
-                    self.scoring(normalize, scores, j, k, 'past')
-                print(scores[3])
-            if group_imp[r][j] < 0 - self.threshold:  # not checking the first test-round
-                self.scoring(normalize, scores, j)
+                    self.scoring_across(normalize, scores, j, k)
         return scores
